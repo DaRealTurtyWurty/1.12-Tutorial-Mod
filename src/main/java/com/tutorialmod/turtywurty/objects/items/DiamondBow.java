e9421502e7bc888877ec37f0d79bcfebd9c3b42b
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2019-05-06 11:54:29 +0100

    Fixed Bow

--- src/main/java/com/tutorialmod/turtywurty/objects/items/DiamondBow.java
+++ src/main/java/com/tutorialmod/turtywurty/objects/items/DiamondBow.java
@@ -1,11 +1,19 @@
 package com.tutorialmod.turtywurty.objects.items;
 
+import javax.annotation.Nullable;
+
 import com.tutorialmod.turtywurty.TutorialMod;
 import com.tutorialmod.turtywurty.init.ItemInit;
 import com.tutorialmod.turtywurty.util.interfaces.IHasModel;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DiamondBow extends ItemBow implements IHasModel
 {
@@ -16,6 +24,30 @@ public class DiamondBow extends ItemBow implements IHasModel
 		setCreativeTab(TutorialMod.TUTORIALMODTAB);
 		setMaxDamage(800);
 		setMaxStackSize(1);
+		
+		this.addPropertyOverride(new ResourceLocation("pull"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                if (entityIn == null)
+                {
+                    return 0.0F;
+                }
+                else
+                {
+                    return entityIn.getActiveItemStack().getItem() != ItemInit.DIAMOND_BOW ? 0.0F : (float)(stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
+                }
+            }
+        });
+        this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            }
+        });
 		
 		ItemInit.ITEMS.add(this);
 	}
