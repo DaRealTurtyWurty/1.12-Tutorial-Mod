07527a68e9ccde5db7d77ca0527749f4434bef71
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2019-05-31 13:32:26 +0100

    Initial Commit

--- src/main/java/com/tutorialmod/turtywurty/world/test/BiomeTest.java
+++ src/main/java/com/tutorialmod/turtywurty/world/test/BiomeTest.java
@@ -14,10 +14,10 @@ public class BiomeTest extends Biome
 {
 	public BiomeTest() 
 	{
-		super(new BiomeProperties("Test").setBaseHeight(0.2f).setSnowEnabled().setTemperature(-5.0f).setHeightVariation(1.2f).setWaterColor(16724639));
+		super(new BiomeProperties("Test").setBaseHeight(0.2f).setTemperature(0.5f).setHeightVariation(1.8f).setWaterColor(16724639));
 		
 		topBlock = BlockInit.SILVER_BLOCK.getDefaultState();
-		fillerBlock = BlockInit.RANDOM_BLOCK.getDefaultState();
+		fillerBlock = BlockInit.SILVER_FURNACE_ON.getDefaultState();
 		
 		//decorator = new BiomeDecoratorTest();
 		this.spawnableCaveCreatureList.clear();
